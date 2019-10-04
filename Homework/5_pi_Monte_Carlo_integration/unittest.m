5243154bc7642b0d929da27cffc9d6d65f150560
scarecrowyu <295131923@qq.com>
2019-10-04 20:24:39 +0800

    .

--- Homework/5_pi_Monte_Carlo_integration/unittest.m
+++ Homework/5_pi_Monte_Carlo_integration/unittest.m
@@ -1,3 +1,3 @@
 %unittest
 hfe = @(x,y) max(max(abs(x-y)./(abs(x)+abs(y)+1e-3)));
-disp(['relative error: ',num2str(hfe(solution(100000),pi),5)])
+disp(['relative error: ',num2str(hfe(code_template(100000),pi),5)])
